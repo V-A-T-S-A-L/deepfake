023330571b1782cc8316be674149ca401492441e
samarthshettyyy <samarthshetty04@gmail.com>
2025-04-21 11:14:34 +0530

    root 1

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Install system dependencies for dlib
+apt-get update && apt-get install -y cmake libgl1-mesa-glx
+
 pip install -r requirements.txt
 
 # collect static files
